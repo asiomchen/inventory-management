22aa5566c6f42a92167dc395ac8822156c6ea3af
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2023-09-23 18:11:49 +0200

    image generator added

---
+++ utils.py
@@ -0,0 +1,13 @@
+from numpy import mat
+from randimage import get_random_image
+from matplotlib import image
+import os
+
+
+def generate_random_image(path: str):
+    if os.path.exists(path):
+        return os.path.basename(path)
+    img_size = (400, 400)
+    img = get_random_image(img_size)
+    image.imsave(path, img)
+    return os.path.basename(path)
