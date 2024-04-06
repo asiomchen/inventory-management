d279efc7452889fb4a6c0739ba86d705f6da175e
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2024-04-06 18:40:02 +0200

    Add logging and error handling to image upload and delivery functions

--- images.py
+++ images.py
@@ -1,10 +1,12 @@
+import logging
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from typing import Tuple
+from flask import url_for, current_app
+from jinja2 import runtime
 
 config = cloudinary.config(secure=True)
-
 
 def upload_image(image_path) -> Tuple[str, str]:
     upload_result = cloudinary.uploader.upload(
@@ -25,6 +27,12 @@ def delete_image(public_id):
 
 
 def deliver_image(public_id, width, height):
+    logging.info(f"public_id: '{public_id}'")
+    logging.info(f"{type(public_id)}")
+    if isinstance(public_id, runtime.Undefined):
+        logging.info("No photo found, using default")
+        return current_app.url_for("static", filename="no-photo.bmp")
+    logging.info(f"Fetching photo from cloudinary: {public_id}")
     return cloudinary.utils.cloudinary_url(
         public_id, width=width, height=height, crop="fill"
     )[0]
